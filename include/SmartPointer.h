ff1f39a05f2282d9b3419d301468f64b0695302a
fangfeng <fangfeng@wacai.com>
2017-08-25 18:10:59 +0800

    fix bug

--- include/SmartPointer.h
+++ include/SmartPointer.h
@@ -1,67 +1,74 @@
+#ifndef	SMART_POINTER_H
+#define SMART_POINTER_H
+
 #include <cstdlib>
 
 template <typename T>  
 class SmartPointer {  
 public:  
     //构造函数  
-    SmartPointer(T* p=0): _ptr(p), _reference_count(new size_t){  
-        if(p)  
-            *_reference_count = 1;   
-        else  
-            *_reference_count = 0;   
-    }  
+    SmartPointer(T* p=0): ptr(p), reference_count(new int(1)){}  
     //拷贝构造函数  
     SmartPointer(const SmartPointer& src) {  
         if(this!=&src) {  
-            _ptr = src._ptr;  
-            _reference_count = src._reference_count;  
-            (*_reference_count)++;  
+            ptr = src.ptr;  
+            reference_count = src.reference_count;  
+            (*reference_count)++;  
         }  
     }  
     //重载赋值操作符  
     SmartPointer& operator=(const SmartPointer& src) {  
-        if(_ptr==src._ptr) {  
+        if(ptr==src.ptr) {  
             return *this;  
         }  
         releaseCount();  
-        _ptr = src._ptr;  
-        _reference_count = src._reference_count;  
-        (*_reference_count)++;  
+        ptr = src.ptr;  
+        reference_count = src.reference_count;  
+        (*reference_count)++;  
         return *this;  
     }  
   
     //重载操作符  
     T& operator*() {  
-        if(_ptr) {  
-            return *_ptr;  
+        if(ptr) {  
+            return *ptr;  
         }  
         //throw exception  
+    }
+
+    const T& operator*() const {
+	if(ptr) {
+		return *ptr;
+	}
     }  
+
     //重载操作符  
     T* operator->() {  
-        if(_ptr) {  
-            return _ptr;  
-        }  
+            return ptr;  
         //throw exception  
-    }  
+    }
+    const T* operator->() const {
+	return ptr;
+    }
+  
     //析构函数  
     ~SmartPointer() {  
-        if (--(*_reference_count) == 0) {  
-            delete _ptr;  
-            delete _reference_count;  
+        if (--(*reference_count) == 0) {  
+            delete ptr;  
+            delete reference_count;  
         }  
     }  
 private:  
-    T *_ptr;  
-        size_t *_reference_count;  
-        void releaseCount() {  
-        if(_ptr) {  
-            (*_reference_count)--;  
-                if((*_reference_count)==0) {  
-                    delete _ptr;  
-                    delete _reference_count;  
-                }  
+    T *ptr;  
+    int *reference_count;  
+    void releaseCount() {  
+        (*reference_count)--;  
+        if((*reference_count)==0) {
+		if(ptr) delete ptr;  
+        	delete reference_count;  
         }  
-        }  
+    }  
 };  
   
+#endif
+
--- main/TestSmartPointer.cpp
+++ main/TestSmartPointer.cpp
@@ -4,12 +4,15 @@ int main()
 {
     SmartPointer<char> cp1(new char('a'));
     SmartPointer<char> cp2(cp1);
-    SmartPointer<char> cp3;
+    SmartPointer<char> cp3 = new char('c');
     cp3 = cp2;
     cp3 = cp1;
     cp3 = cp3;
     SmartPointer<char> cp4(new char('b'));
     cp3 = cp4;
+    SmartPointer<char> cp5;
+    SmartPointer<char> cp6 = cp5;
+
     return 0;
 }
 
